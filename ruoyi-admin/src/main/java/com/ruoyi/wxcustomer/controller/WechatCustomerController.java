05d39bfde621c934e6a189e1e2f625e8fb3bd444
lixianglife <a2872499043@163.com>
2019-10-24 09:46:19 +0800

    微信客户生成代码

---
+++ ruoyi-admin/src/main/java/com/ruoyi/wxcustomer/controller/WechatCustomerController.java
@@ -0,0 +1,116 @@
+package com.ruoyi.wxcustomer.controller;
+
+import java.util.List;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.wxcustomer.domain.WechatCustomer;
+import com.ruoyi.wxcustomer.service.IWechatCustomerService;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
+
+/**
+ * 微信客户Controller
+ * 
+ * @author yzh
+ * @date 2019-10-23
+ */
+@Controller
+@RequestMapping("/wxcustomer/WechatCustomer")
+public class WechatCustomerController extends BaseController{
+    private String prefix = "wxcustomer/WechatCustomer";
+
+    @Autowired
+    private IWechatCustomerService wechatCustomerService;
+
+    @RequiresPermissions("wxcustomer:WechatCustomer:view")
+    @GetMapping()
+    public String WechatCustomer(){
+        return prefix + "/WechatCustomer";
+    }
+
+    /**
+     * 查询微信客户列表
+     */
+    @RequiresPermissions("wxcustomer:WechatCustomer:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(WechatCustomer wechatCustomer){
+        startPage();
+        List<WechatCustomer> list = wechatCustomerService.selectWechatCustomerList(wechatCustomer);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出微信客户列表
+     */
+    @RequiresPermissions("wxcustomer:WechatCustomer:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(WechatCustomer wechatCustomer){
+        List<WechatCustomer> list = wechatCustomerService.selectWechatCustomerList(wechatCustomer);
+        ExcelUtil<WechatCustomer> util = new ExcelUtil<WechatCustomer>(WechatCustomer.class);
+        return util.exportExcel(list, "WechatCustomer");
+    }
+
+    /**
+     * 新增微信客户
+     */
+    @GetMapping("/add")
+    public String add(){
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存微信客户
+     */
+    @RequiresPermissions("wxcustomer:WechatCustomer:add")
+    @Log(title = "微信客户", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(WechatCustomer wechatCustomer){
+        return toAjax(wechatCustomerService.insertWechatCustomer(wechatCustomer));
+    }
+
+    /**
+     * 修改微信客户
+     */
+    @GetMapping("/edit/{customerId}")
+    public String edit(@PathVariable("customerId") String customerId, ModelMap mmap){
+        WechatCustomer wechatCustomer = wechatCustomerService.selectWechatCustomerById(customerId);
+        mmap.put("wechatCustomer", wechatCustomer);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存微信客户
+     */
+    @RequiresPermissions("wxcustomer:WechatCustomer:edit")
+    @Log(title = "微信客户", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(WechatCustomer wechatCustomer) {
+        return toAjax(wechatCustomerService.updateWechatCustomer(wechatCustomer));
+    }
+
+    /**
+     * 删除微信客户
+     */
+    @RequiresPermissions("wxcustomer:WechatCustomer:remove")
+    @Log(title = "微信客户", businessType = BusinessType.DELETE)
+    @PostMapping( "/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        return toAjax(wechatCustomerService.deleteWechatCustomerByIds(ids));
+    }
+}
---
+++ ruoyi-admin/src/main/java/com/ruoyi/wxcustomer/service/IWechatCustomerService.java
@@ -0,0 +1,60 @@
+package com.ruoyi.wxcustomer.service;
+
+import com.ruoyi.wxcustomer.domain.WechatCustomer;
+import java.util.List;
+
+/**
+ * 微信客户Service接口
+ * 
+ * @author yzh
+ * @date 2019-10-23
+ */
+public interface IWechatCustomerService {
+    /**
+     * 查询微信客户
+     * 
+     * @param customerId 微信客户ID
+     * @return 微信客户
+     */
+    public WechatCustomer selectWechatCustomerById(String customerId);
+
+    /**
+     * 查询微信客户列表
+     * 
+     * @param wechatCustomer 微信客户
+     * @return 微信客户集合
+     */
+    public List<WechatCustomer> selectWechatCustomerList(WechatCustomer wechatCustomer);
+
+    /**
+     * 新增微信客户
+     * 
+     * @param wechatCustomer 微信客户
+     * @return 结果
+     */
+    public int insertWechatCustomer(WechatCustomer wechatCustomer);
+
+    /**
+     * 修改微信客户
+     * 
+     * @param wechatCustomer 微信客户
+     * @return 结果
+     */
+    public int updateWechatCustomer(WechatCustomer wechatCustomer);
+
+    /**
+     * 批量删除微信客户
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteWechatCustomerByIds(String ids);
+
+    /**
+     * 删除微信客户信息
+     * 
+     * @param customerId 微信客户ID
+     * @return 结果
+     */
+    public int deleteWechatCustomerById(String customerId);
+}
