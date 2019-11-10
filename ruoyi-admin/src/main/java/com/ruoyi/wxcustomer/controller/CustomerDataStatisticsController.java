a0c6ca0ce61e44a7036e86bd863f75be52664c1d
HUASHENGHE <1594213510@qq.com>
2019-11-10 20:24:52 +0800

    修改

--- ruoyi-admin/src/main/java/com/ruoyi/wxcustomer/controller/CustomerDataStatisticsController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/wxcustomer/controller/CustomerDataStatisticsController.java
@@ -76,7 +76,7 @@ public class CustomerDataStatisticsController extends BaseController {
 			}
 		}
 		vo.setIsDelete("0");
-		if (StringUtils.isNoneEmpty(vo.getDealTimeStart()) && StringUtils.isNoneEmpty(vo.getDealTimeEnd())) {
+		if (StringUtils.isEmpty(vo.getDealTimeStart()) && StringUtils.isEmpty(vo.getDealTimeEnd())) {
 			vo.setDealTime(new Date());
 		}
 		return getDataTable(khAfterSaleMemberService.selectList(vo));
